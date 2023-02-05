d18970c7d5a49b3a469d7f8f785eec3e3954e338
Nadiia Nesterova <nesterova1717@gmail.com>
2023-02-05 14:28:20 +0200

    feat: update src/guessing-game.js file

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,24 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
-    }
-
+        this.mid =  Math.round((this.min + this.max) / 2 );
+        return this.mid;
+       }
+  
     lower() {
-
+      return this.max = this.mid;
     }
-
+  
     greater() {
-
+      return this.min = this.mid;
     }
-}
+  }
 
 module.exports = GuessingGame;
